c1a1d9a840f5f3120ed720e27c40f04f30ac38ec
terry <ataeteruhito@gmail.com>
2024-11-04 01:56:32 +0900

    change scheme

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -10,13 +10,9 @@ cd "$CI_PRIMARY_REPOSITORY_PATH"
 defaults write com.apple.dt.Xcode EnableCodeCoverage YES
 defaults write com.apple.dt.XCTest EnableCodeCoverage YES
 
-# スキーム名の取得
-SCHEME_NAME=${CI_XCODE_SCHEME:-$(xcodebuild -list | grep -A 1 "Schemes:" | tail -n 1 | xargs)}
-echo "Using scheme: $SCHEME_NAME"
-
 # ビルド設定の確認
 echo "Checking build settings..."
-xcodebuild -scheme "$SCHEME_NAME" \
+xcodebuild -scheme "Production" \
   -destination 'platform=iOS Simulator,name=iPhone 16 Pro,OS=latest' \
   -showBuildSettings | grep -i "coverage"
 
