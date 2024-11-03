7d8bb988e98eca9175b891d206c7136ddf029317
terry <ataeteruhito@gmail.com>
2024-11-03 22:43:36 +0900

    minimum check

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,64 +1,7 @@
-echo "Starting SonarCloud coverage upload process..."
+#!/bin/bash
+set -e
 
-brew install sonar-scanner
-echo "Installed sonar-scanner"
-
-# デバッグ出力を追加
-echo "Creating sonar-project.properties with:"
-echo "Project Key: $SONAR_PROJECT_KEY"
-echo "Organization: $SONAR_ORGANIZATION"
-
-# sonar-project.propertiesの作成
-cat > sonar-project.properties << EOF
-sonar.projectKey=${$SONAR_PROJECT_KEY}
-sonar.organization=${$SONAR_ORGANIZATION}
-sonar.host.url=https://sonarcloud.io
-
-sonar.sources=$CI_PRIMARY_REPOSITORY_PATH
-sonar.swift.coverage.reportPaths=coverage.txt
-sonar.exclusions=**/*.generated.swift,**/Pods/**/*,**/*.pb.swift
-sonar.swift.file.suffixes=.swift
-sonar.sourceEncoding=UTF-8
-sonar.projectVersion=${CI_BUILD_NUMBER}
-sonar.projectName=${CI_PROJECT_NAME}
-EOF
-
-cd $CI_WORKSPACE
-
-# declare variables
-SCHEME="BigIntExtensionsTests"
-PRODUCT_NAME="SumNote"
-WORKSPACE_NAME=${PRODUCT_NAME}.xcworkspace
-APP_VERSION=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' ./${PRODUCT_NAME}.xcodeproj/project.pbxproj)
-
-echo $APP_VERSION
-# clean, build and test project
-xcodebuild \
-  -workspace ${WORKSPACE_NAME} \
-  -destination 'platform=iOS Simulator,name=iPhone 16 Pro,OS=latest' \
-  -scheme ${SCHEME} \
-  -derivedDataPath DerivedData/ \
-  -enableCodeCoverage YES \
-  -resultBundlePath DerivedData/Logs/Test/ResultBundle.xcresult \
-  clean build test
-
-# find profdata and binary
-PROFDATA=$(find . -name "Coverage.profdata")
-BINARY=$(find . -path "*${PRODUCT_NAME}.app/${PRODUCT_NAME}")
-
-# check if we have profdata file
-if [[ -z $PROFDATA ]]; then
-  echo "ERROR: Unable to find Coverage.profdata. Be sure to execute tests before running this script."
-  exit 1
-fi
-
-# extract coverage data from project using xcode native tool
-xcrun xccov view --report "$XCRESULT_PATH" > coverage.txt
-xcrun --run llvm-cov show -instr-profile=${PROFDATA} ${BINARY} > coverage.txt
-
-# SonarCloudスキャンの実行
-sonar-scanner \
-  -Dsonar.token="$SONAR_TOKEN" \
-  -Dsonar.working.directory=.scannerwork
-  
-echo "Completed SonarCloud upload"
+# Xcode Cloud環境変数のデバッグ出力
+echo "Checking environment variables..."
+echo "CI_DERIVED_DATA_PATH: $CI_DERIVED_DATA_PATH"
+echo "CI_PRIMARY_REPOSITORY_PATH: $CI_PRIMARY_REPOSITORY_PATH"
