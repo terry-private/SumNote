a96d65a5f39f1c645243a094518ec5062ba10a08
terry <ataeteruhito@gmail.com>
2024-11-04 16:56:12 +0900

    fix

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ $CI_WORKFLOW = "SonarCloud Upload" ]; then
+if [ "$CI_WORKFLOW" = "SonarCloud Upload" ]; then
   ./sonar_cloud_upload.sh
 else
   echo dont upload cause $CI_WORKFLOW is not "SonarCloud Upload"
