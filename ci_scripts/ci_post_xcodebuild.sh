7fef8c49a0fb4f5d1e59b8af05360bc5637eec5e
terry <ataeteruhito@gmail.com>
2024-11-18 07:33:57 +0900

    change work flow name

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,5 +4,5 @@ set -e
 if [ "$CI_WORKFLOW" = "SonarCloud Upload" ]; then
   ./sonar_cloud_upload.sh
 else
-  echo dont upload cause $CI_WORKFLOW is not "SonarCloud Upload"
+  echo dont upload cause $CI_WORKFLOW is not "[inspect]SonarCloud Upload"
 fi
