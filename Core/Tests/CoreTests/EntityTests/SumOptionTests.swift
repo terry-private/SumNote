029c8a4fbce527c099ccfc0e8a3f978d46525d18
terry <ataeteruhito@gmail.com>
2024-11-25 08:50:14 +0900

    add DescriptionWithIndex test

--- Core/Tests/CoreTests/EntityTests/SumOptionTests.swift
+++ Core/Tests/CoreTests/EntityTests/SumOptionTests.swift
@@ -8,7 +8,13 @@ struct SumOptionTests {
             "description"
         )
         func description() {
-            #expect(SumOption(name: "", ratio: .init(1, 2)).description == "x 0.5 ()")
+            #expect(SumOption(name: "sample", ratio: .init(1, 2)).description == "x 0.5 (sample)")
+        }
+    }
+    struct DescriptionWithIndex {
+        @Test("description with index")
+        func descriptionWithIndex() {
+            #expect(SumOption(name: "sample", ratio: .init(1, 2)).description(with: 1) == "  x 0.5 (sample)")
         }
     }
 }
